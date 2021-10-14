e1df3ee814f388fd9b19895fe9dfda4dc5c375f2
Mikita86 <mikithykoleg888@gmail.com>
2021-10-14 22:53:11 +0300

    tasks part 3

--- codeComp.js
+++ codeComp.js
@@ -32,3 +32,18 @@ myLocalScope();
 // Run and check the console
 // myVar is not defined outside of myLocalScope
 console.log('outside myLocalScope', myVar);
+
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+    // Only change code below this line
+
+    var outerWear = "sweater";
+
+    // Only change code above this line
+    return outerWear;
+}
+
+myOutfit();
