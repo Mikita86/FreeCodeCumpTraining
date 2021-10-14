a1c9329479f6005cef251b6564ad89bed52be4bf
Mikita86 <mikithykoleg888@gmail.com>
2021-10-14 19:17:21 +0300

    tasks part 2

--- codeComp.js
+++ codeComp.js
@@ -20,3 +20,15 @@ function fun2() {
 }
 var myGlobal = 10;
 oopsGlobal = 5;
+
+function myLocalScope() {
+
+    // Only change code below this line
+    var myVar;
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
