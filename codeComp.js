6f25cf35a8c3aa08dbf874f94169d6ee9d8abfe1
Mikita86 <mikithykoleg888@gmail.com>
2021-10-17 17:46:41 +0300

    tasks part 6

--- codeComp.js
+++ codeComp.js
@@ -72,3 +72,15 @@ function addThree() {
 
 addThree();
 addFive();
+
+
+// Setup
+var processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+// Only change code below this line
+
+processed = processArg(7);
