a29333a831f1e2523ba903d9a73127c22f5c47fd
Mikita86 <mikithykoleg888@gmail.com>
2021-10-21 20:58:44 +0300

    tasks part 18

--- codeComp.js
+++ codeComp.js
@@ -233,3 +233,18 @@ function testLogicalAnd(val) {
 
 
 testLogicalAnd(10);
+
+
+function testLogicalOr(val) {
+    // Only change code below this line
+
+    if (val > 20 || val < 10) {
+        return "Outside";
+    }
+
+
+    // Only change code above this line
+    return "Inside";
+}
+
+testLogicalOr(15);
