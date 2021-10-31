16753a6322c43bee8e67e7224953741f98540742
Mikita86 <mikithykoleg888@gmail.com>
2021-10-31 21:02:04 +0300

    tasks part 34

--- codeComp.js
+++ codeComp.js
@@ -462,3 +462,14 @@ function chainToSwitch(val) {
     //   answer = "Ate Nine";
     // }
 }
+
+
+
+function isLess(a, b) {
+    // Only change code below this line
+    return a < b;
+
+    // Only change code above this line
+}
+
+isLess(10, 15);
