4a9ff622c3d9f713013037fc619dc1d95f60a86a
Mikita86 <mikithykoleg888@gmail.com>
2021-11-02 21:25:27 +0300

    tasks part 38

--- codeComp.js
+++ codeComp.js
@@ -527,6 +527,18 @@ const myDog = {
 
 
 
+// Setup
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
+
+
 
 
 cc(2);
