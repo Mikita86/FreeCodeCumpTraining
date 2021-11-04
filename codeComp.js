8968cc5ea325d576b443ede1ce1489d108dc263f
Mikita86 <mikithykoleg888@gmail.com>
2021-11-04 21:31:20 +0300

    tasks part 42

--- codeComp.js
+++ codeComp.js
@@ -589,3 +589,16 @@ const myDog = {
 };
 
 myDog.bark = 'woof';
+
+
+
+// Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+delete myDog.tails;
+// Only change code below this line
