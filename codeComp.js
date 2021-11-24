626139efc800be89ff4a51674eead3fd81ed3c6a
Mikita86 <mikithykoleg888@gmail.com>
2021-11-24 21:05:21 +0300

    second update

--- codeComp.js
+++ codeComp.js
@@ -601,3 +601,24 @@ const myDog = {
     "bark": "woof"
 };
 delete myDog.tails;
+
+
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+
+    // Only change code below this line
+    const lookup = {
+        alpha: "Adams",
+
+        bravo: "Boston",
+        charlie: "Chicago",
+        delta: "Denver",
+        echo: "Easy",
+        foxtrot: "Frank",
+    };
+
+    // Only change code above this line
+    result = lookup[val];
+    return result;
+};
